2c5d431773f432824fdbc192123a06be946856e1
Ezzat Qupty <ezzat.qupty12345@gmail.com>
2019-04-16 18:00:44 +0300

    Initial Commit

---
+++ Data Structures HW2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structures HW2
+//
+//  Created by Ezzat Qupty on 16/04/2019.
+//  Copyright © 2019 Ezzat Qupty. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
